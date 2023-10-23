29216deaef02db6c21260ba64cd7235491a058bb
Artur Aghajanyan <aghajanyan.aa1@gmail.com>
2023-10-23 18:41:44 -0400

    Changed String values to properties

--- src/main/java/art/pricetracker/service/EmailSenderService.java
+++ src/main/java/art/pricetracker/service/EmailSenderService.java
@@ -1,6 +1,7 @@
 package art.pricetracker.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -10,16 +11,21 @@ public class EmailSenderService {
     @Autowired
     private JavaMailSender mailSender;
 
+    @Value("${spring.mail.username}")
+    private String senderUserName;
+
+    private final String messageSubject = "Price changed";
+
     private EmailSenderService(JavaMailSender mailSender) {
         this.mailSender = mailSender;
     }
 
     public void sendSimpleEmail(String toEmail, String body) {
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("***");
+        message.setFrom(senderUserName);
         message.setTo(toEmail);
         message.setText(body);
-        message.setSubject("Price changed");
+        message.setSubject(messageSubject);
         mailSender.send(message);
     }
 }
